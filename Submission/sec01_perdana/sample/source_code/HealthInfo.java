29ce12d1435c6f53eef93bd13ca4961bdb156268
Lizawati Mi Yusuf <138539895+jjn7702@users.noreply.github.com>
2023-12-20 17:57:32 +0800

    Add files via upload

---
+++ Submission/sec01_perdana/sample/source_code/HealthInfo.java
@@ -0,0 +1,16 @@
+class HealthInfo {
+	private int height, weight;
+
+	public HealthInfo(int h, int w) {
+		height = h;
+		weight = w;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+}
