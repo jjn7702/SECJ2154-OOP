6427972bc21e691569bdab747d90f2556d633eed
weiyang0412 <77320530+weiyang0412@users.noreply.github.com>
2023-12-24 18:51:23 +0000

    Sarveish, have a look at some changes i did, Book Class done as of now in terms of its methods and etc. Double check brader

--- Submission/sec01_perdana/Group3/source_code/src/Admin.java
+++ Submission/sec01_perdana/Group3/source_code/src/Admin.java
@@ -1,11 +1,14 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 class Admin extends User {
     private Vector<Customer> customers;
+    private static Vector<Book> books;
     public Admin(String id, String name ,String pw, String mail, int roleID, String fName, String lName){
         super(id, name, pw, mail, roleID,fName,lName);
         customers = new Vector<Customer>();
+        books = new Vector<Book>(); //double check with sarveish.
     }
 
     public Admin(){}
@@ -13,7 +16,31 @@ class Admin extends User {
     public void viewAllCustomers(Customer c) throws FileNotFoundException{
         customers = c.getCustomersfromFile();
         c.viewAllCustomers(customers);
-        
+    }
+
+    // public void viewAllBooks(Book b,int roleID) throws FileNotFoundException{ //New function sar, double check nanti :)
+    //     books = b.getBooksfromFile();
+    //     b.viewAllBooks(roleID);
+    // }
+
+    public void manageBookOperation(Book b, int option, int roleID) throws IOException{
+        books = b.getBooksfromFile();
+        switch (option) {
+            case 1:
+                b.addBooksIntoFile(books);
+                break;
+            case 2:
+                b.removeBookFromFile(books); //stopped here, sarveish, kasi continue from here if u feel anything u bole improve.
+                break;
+            case 3:
+                b.updateBookMenu(1);
+                break;
+            case 4:
+                b.viewAllBooks(roleID); //to view book catalogue.
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
