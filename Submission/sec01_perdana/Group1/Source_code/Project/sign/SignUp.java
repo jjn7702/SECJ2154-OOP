dd0ea2e56a865178c5b05c35dd839340cb97d158
mnizuddin <154438734+mnizuddin@users.noreply.github.com>
2023-12-29 22:33:42 +0800

    Update SignUp.java

--- Submission/sec01_perdana/Group1/Source_code/Project/sign/SignUp.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/sign/SignUp.java
@@ -1,18 +1,29 @@
 class SignUp {
   
-  private String email, username, password;
-  private int contact_number;
+  private String email, gender, username, password;
+  private Address add;
+  private int phone;
 
-  public SignUp (String email, String username, String password, int contact_number) {
+  public SignUp (String email, String gender, String username, String password, Address add, int phone) {
     this.email = email;
+    this.gender = gender;
+    Address = add;
     this.username = username;
     this.password = password;
-    this.contact_number = contact;
+    this.phone = phone;
   }
-
+  
   public String getEmail(){
     return email;
   }
+
+  public String getGender(){
+    return gender;
+  }
+
+  public Address getAdd(){
+    return add;
+  } 
 
   public String getUsername(){
     return username;
@@ -22,6 +33,6 @@ class SignUp {
     return password;
   }
 
-  public int getContact_number(){
-    return contact;
+  public int getPhone(){
+    return phone;
   } 
