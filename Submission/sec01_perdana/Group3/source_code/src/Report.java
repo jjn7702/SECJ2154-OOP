13c3dace56785692449273252ac4433ece593424
cheesen <78629638+tancheesen123@users.noreply.github.com>
2023-12-29 07:48:21 +0000

    Add report

---
+++ Submission/sec01_perdana/Group3/source_code/src/Report.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Report{
+    private Vector<OrderManagement> orderList;
+
+    public Report(Vector<OrderManagement>orderList){
+        this.orderList = orderList;
+    }
+
+    public Report(){
+
+    }
+
+    public void generateReport(){
+
+    }
+
+    public void getOrderList(){
+        
+    }
+}
