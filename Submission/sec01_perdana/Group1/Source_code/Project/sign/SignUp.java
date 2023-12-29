44731f0157db094bb94f1768789197705be899c8
mnizuddin <154438734+mnizuddin@users.noreply.github.com>
2023-12-29 22:06:05 +0800

    Create SignUp.java

---
+++ Submission/sec01_perdana/Group1/Source_code/Project/sign/SignUp.java
@@ -0,0 +1,27 @@
+class SignUp{
+  private String email, username, password;
+  private int contact_number;
+
+  public SignUp (String email, String username, String password, int contact_number){
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.contact_number = contact;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public String getUsername(){
+    return username;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+
+  public String getContact_number(){
+    return contact;
+  }
+    
