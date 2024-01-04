59a1b7022c0c461a8c0df54ea5f2081f54e01380
IqbalNotAvailable <154432968+IqbalNotAvailable@users.noreply.github.com>
2024-01-04 13:33:36 +0800

    Update Job.java

--- Submission/sec01_perdana/Group1/Source_code/Project/job/joblist/Job.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/job/joblist/Job.java
@@ -1,1 +1,15 @@
+class Job {
+    private String title, description;
+    private double pay;
+    Company company;
 
+    public Job(String title, double pay){
+        this.title = title;
+        this.pay = pay;
+    }
+
+    String getTitle(){return title;}
+    double getPay(){return pay;}
+    String getDescription(){return description;}
+
+}
