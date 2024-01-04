f5519e286207635712e818e5b1300dfcae028608
IqbalNotAvailable <154432968+IqbalNotAvailable@users.noreply.github.com>
2024-01-04 18:35:05 +0800

    Update main.java

--- Submission/sec01_perdana/Group1/Source_code/Project/main.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/main.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 class main {
   public static void main (String[] args){
+    String fileName = job.txt;
+    List<Job> jobs = JobReader.readJob(fileName);
+    
     System.out.println("JOB PORTAL");
+
+    System.out.println("Hiring");
+    for(Job j : jobs){
+      System.out.println("Job Title: " + j.getJobTitle() + "\nPay: RM " j.getPay());
+    }    
   }
 }
