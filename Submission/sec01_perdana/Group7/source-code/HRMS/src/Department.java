58733418c83413f8583b60cba213c42f0e10a8da
002diello <aidielirhan2002@gmail.com>
2024-01-09 14:57:11 +0800

    Department Class

--- Submission/sec01_perdana/Group7/source-code/HRMS/src/Department.java
+++ Submission/sec01_perdana/Group7/source-code/HRMS/src/Department.java
@@ -1,3 +1,29 @@
-public class Department {
-    
+class Department {
+    private int departmentID;
+    private String departmentName;
+    private HRManager manager;
+    private List<Staff> staffMembers;
+
+    public Department(int departmentID, String departmentName, HRManager manager) {
+        this.departmentID = departmentID;
+        this.departmentName = departmentName;
+        this.manager = manager;
+        this.staffMembers = new ArrayList<>();
+    }
+
+    public void addStaffMember(Staff staff) {
+        staffMembers.add(staff);
+    }
+
+    public void removeStaffMember(Staff staff) {
+        staffMembers.remove(staff);
+    }
+
+    public void displayDepartmentInfo() {
+        System.out.println("Department ID: " + departmentID);
+        System.out.println("Department Name: " + departmentName);
+        System.out.println(
+                "Manager: " + manager.getEmployee().getFirstName() + " " + manager.getEmployee().getLastName());
+        System.out.println("Number of Staff Members: " + staffMembers.size());
+    }
 }
