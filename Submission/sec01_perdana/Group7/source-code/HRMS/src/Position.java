6e7103babaec45cf7984606cef7b908db3aa3f7c
002diello <aidielirhan2002@gmail.com>
2024-01-09 15:02:11 +0800

    Class Position

---
+++ Submission/sec01_perdana/Group7/source-code/HRMS/src/Position.java
@@ -0,0 +1,12 @@
+class Position {
+    private int positionID;
+    private String title;
+    private String responsibilities;
+
+    public Position(int positionID, String title, String responsibilities) {
+        this.positionID = positionID;
+        this.title = title;
+        this.responsibilities = responsibilities;
+    }
+
+}
