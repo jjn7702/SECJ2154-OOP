72a7081e218e14ba3f936ecc8c699020c37bbd71
Anushka Roshni Sivakumar <82142164+anushkagemini@users.noreply.github.com>
2024-01-11 17:13:58 +0800

    Update Book.java

--- Submission/sec01_perdana/Group 8/sourcecode/Book.java
+++ Submission/sec01_perdana/Group 8/sourcecode/Book.java
@@ -1,46 +1,110 @@
+import java.util.Vector;
+
 public class Book {
+    private String bookID;
     private String title;
-    private String genre;
     private String ISBN;
-    private Fiction fiction;
-    private NonFiction nonFiction;
-    private Catalog catalog;
+    private int quantity;
+    private int genre;
+    private String author;
 
-    // Constructors, getters, and setters
+    public Book(){
+    }
 
-    public Book(String title, String genre, String ISBN, Fiction fiction, NonFiction nonFiction, Catalog catalog){
+    public Book(String title, String bookID, String ISBN, int quantity, int genre, String author){
         this.title = title;
-        this.genre = genre;
+        this.bookID = bookID;
         this.ISBN = ISBN;
-        this.fiction = fiction;
-        this.nonFiction = nonFiction;
-        this.catalog = catalog;
+        this.quantity = quantity;
+        this.genre =  genre;
+        this.author = author;
     }
 
-    public Catalog geCatalog(){
-        return catalog;
+    public String getBookID(){
+        return bookID;
     }
 
-    public class Fiction extends Book {
-        public Fiction(String title, String genre, String ISBN, Catalog catalog) {
-            super(title, genre, ISBN, fiction, nonFiction, catalog);
+    public String getTitle(){
+        return title;
+    }
+
+    public String getISBN(){
+        return ISBN;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public int getGenre(){
+        return genre;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setBookID(String bookID){
+        this.bookID = bookID;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setISBN(String ISBN){
+        this.ISBN = ISBN;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public void setGenre(int genre){
+        this.genre = genre;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void display(){
+        System.out.println("Book ID: " + bookID);
+        System.out.println("Title: " + title);
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Genre: " + genre);
+        System.out.println("Author: " + author);
+    }
+
+    public void addBookToLibrary(Vector<Book> library) {
+        library.add(this);
+    }
+
+    public void updateBookQuantity(Vector<Book> library, int newQuantity) {
+        for (Book book : library) {
+            if (book.getBookID().equals(this.bookID)) {
+                book.setQuantity(newQuantity);
+                break;
+            }
         }
     }
 
-    public class NonFiction extends Book /*implements Discount*/ {
-        //private double discountRate;
-    
-        public NonFiction(String title, String genre, String ISBN, Catalog catalog, double discountRate) {
-            super(title, genre, ISBN, fiction, nonFiction, catalog);
-            //this.discountRate = discountRate;
-        }
-    
-       
-        /*public double getDiscount() {
-            return discountRate;
-        }*/
-
+    public void removeBook(Vector<Book> library) {
+        library.remove(this);
     }
 
+    public void updateBookDetails(Vector<Book> library) {
+        for (Book book : library) {
+            if (book.getBookID().equals(this.bookID)) {
+                book.setTitle(this.title);
+                book.setISBN(this.ISBN);
+                book.setQuantity(this.quantity);
+                book.setGenre(this.genre);
+                book.setAuthor(this.author);
+                break;
+            }
+        }
+    }
 }
 
