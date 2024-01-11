d918a23c4296c865a7effdc289a97ccdd373707f
Mnqarlz <aiman020404@gmail.com>
2024-01-11 21:40:11 +0800

    update attendance class

--- Submission/sec01_perdana/Group7/source-code/HRMS/src/Attendance.java
+++ Submission/sec01_perdana/Group7/source-code/HRMS/src/Attendance.java
@@ -1,20 +1,47 @@
-// NAME : MUHAMMAD AIMAN HAIQAL BIN SALEHUDDIN
 class Attendance {
     private int attendanceID;
     private Employee employee;
     private String date;
     private double hoursWorked;
-    private final double REGULARHOURS = 9.0;
 
-    public Attendance(int aID, Employee employee, String dt, double hW){
-        this.attendanceID = aID;
+    public Attendance(int attendanceID, Employee employee, String date, double hoursWorked) {
+        this.attendanceID = attendanceID;
         this.employee = employee;
-        this.date = dt;
-        this.hoursWorked = hW;
+        this.date = date;
+        this.hoursWorked = hoursWorked;
     }
 
     public double calculateOvertime() {
-        double overtimeHours = Math.max(0, hoursWorked - REGULARHOURS);
+        double regularHours = 8.0;
+        double overtimeHours = Math.max(0, hoursWorked - regularHours);
         return overtimeHours;
+    }
+
+    public int getAttendanceID() {
+        return attendanceID;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public String getEmployeeFirstName() {
+        return employee.getFirstName();
+    }
+
+    public String getEmployeeLastName() {
+        return employee.getLastName();
+    }
+
+    public double getEmployeeHoursWorked() {
+        return hoursWorked;
     }
 }
