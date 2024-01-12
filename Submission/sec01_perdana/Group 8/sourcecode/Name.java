439cfe8e7190dbeeeb80e8ebde71cdbbaf87f08c
Danesh Muthu Krisnan <37767781+DaneshIV@users.noreply.github.com>
2024-01-12 09:25:58 +0000

    Update User , UserDatabase & Menu

---
+++ Submission/sec01_perdana/Group 8/sourcecode/Name.java
@@ -0,0 +1,26 @@
+class Name{
+    private String fName;
+    private String lName;
+
+    public Name(fName, lName){
+        this.fName = fName;
+        this.lName = lName;
+    }
+
+    public String getfName(){
+        return fName;
+    }
+
+    public String getlName(){
+        return lName;
+    }
+
+    public void setfName(String fName){
+        this.fName = fName
+    }
+
+    public void setlName(String lName){
+        this.lName = lName;
+    }
+
+}
