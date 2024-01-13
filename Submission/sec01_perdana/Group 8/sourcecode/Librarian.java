a137b7782e4717b8d87b42b3bc499716d2118159
haloitsafiq <110480896+haloitsafiq@users.noreply.github.com>
2024-01-13 08:06:27 +0000

    Update Librarian and Report java

--- Submission/sec01_perdana/Group 8/sourcecode/Librarian.java
+++ Submission/sec01_perdana/Group 8/sourcecode/Librarian.java
@@ -1,3 +1,47 @@
+import java.util.*;
+
 class Librarian {
-    
+    Vector <Member> member = new Vector<Member>();
+    Vector <Borrow> borrow = new Vector<Borrow>();
+    private Report report;
+
+    public Librarian(String userID, String userName, String password, int userRole , String fName, String lName, Vector<Book> Book){
+        this.userID = userID;
+        this.userName = userName;
+        this.password = password;
+        this.userRole = userRole;
+        name = new Name(fName,lName); 
+        
+    }
+
+    public Librarian(){}
+
+    protected static viewMenu(){
+        
+    }
+
+    public void viewAllMembers(){
+
+    }
+
+    public static void manageBookOperation(Vector<Book> Book , int , int){
+
+    }
+
+    public void generateReport(Report report){
+
+    }
+
+    public void addBorrowedBooks(Vector<Borrow> borrow) {
+        borrow.add(this);
+    }
+
+    public static Vector<Book> getBook(){
+        return book;
+    }
+
+    public static Vector<Member> getMember(){
+        return member;
+    }
+
 }
---
+++ Submission/sec01_perdana/Group 8/sourcecode/Report.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Report {
+    Vector <Borrow> BorrowList = new Vector<Borrow>();
+
+    public Report(Vector<Borrow> borrow){
+    
+    }
+
+    public Report(){}
+
+    public void generateReport(){
+        ???
+    }
+
+    public void getBorrowList(){
+        return borrow;
+    }
+}  
