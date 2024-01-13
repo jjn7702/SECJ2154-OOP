1bcc14ef00d9920c7a428650608666834ff10577
IqbalNotAvailable <154432968+IqbalNotAvailable@users.noreply.github.com>
2024-01-13 23:40:24 +0800

    Update JobPortal.java

--- Submission/sec01_perdana/Group1/Source_code/Project/JobPortal.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/JobPortal.java
@@ -1,1 +1,18 @@
+import java.util.*;
+import java.io.*;
+import employer.Employer;
+import job_application.JobApplication;
+import job_posting.JobPosting;
+import jobseeker.JobSeeker;
+import portal_exception.PortalException;
+import sign_in.SignIn;
+import sign_up.SignUp;
+import user.User;
+
+public class JobPortal {
+  private List<User> users;
+  private List<JobPosting> jobPostings;
+  private List<JobApplication> jobApplications;
+
+  private static final String USER_FILE_PATH = "UserList.txt";
 
