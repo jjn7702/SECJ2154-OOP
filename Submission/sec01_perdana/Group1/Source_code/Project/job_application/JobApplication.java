46b0005895cd89eb2fb534960da53576dbee9b03
shahzriemg <154407212+shahzriemg@users.noreply.github.com>
2024-01-13 23:05:28 +0800

    Update JobApplication.java

--- Submission/sec01_perdana/Group1/Source_code/Project/job_application/JobApplication.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/job_application/JobApplication.java
@@ -1,1 +1,15 @@
+package job_application;
+import job_posting.JobPosting;
+import jobseeker.JobSeeker;
 
+public class JobApplication {
+    private JobSeeker applicant;
+    private JobPosting job;
+    private String status;
+
+    public JobApplication(JobSeeker applicant, JobPosting job, String status) {
+        this.applicant = applicant;
+        this.job = job;
+        this.status = status;
+    }
+}
