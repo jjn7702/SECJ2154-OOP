bdb87d68bd848d6dc24b17281923197d3323028d
shahzriemg <154407212+shahzriemg@users.noreply.github.com>
2024-01-13 23:07:06 +0800

    Update JobSeeker.java

--- Submission/sec01_perdana/Group1/Source_code/Project/jobseeker/JobSeeker.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/jobseeker/JobSeeker.java
@@ -1,1 +1,16 @@
+package jobseeker;
+import user.User;
+
+public class JobSeeker extends User {
+    private String resume;
+
+    public JobSeeker(String username, String password, String resume) {
+        super(username, password);
+        this.resume = resume;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+}
 
