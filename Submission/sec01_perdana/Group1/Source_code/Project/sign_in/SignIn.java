5a4f2b4793ce99ba5df2a99a2c82db00432bbd69
shahzriemg <154407212+shahzriemg@users.noreply.github.com>
2024-01-13 23:08:21 +0800

    Update SignIn.java

--- Submission/sec01_perdana/Group1/Source_code/Project/sign_in/SignIn.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/sign_in/SignIn.java
@@ -1,1 +1,22 @@
+package sign_in;
+import java.util.*;
+import user.User;
 
+public class SignIn{
+    private List<User> users;
+
+    public SignIn(List<User> users) {
+        this.users = users;
+    }
+
+    public User signIn(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                System.out.println("Sign in successful!");
+                return user;
+            }
+        }
+        System.out.println("Invalid username or password. Sign in failed.");
+        return null;
+    }
+}
