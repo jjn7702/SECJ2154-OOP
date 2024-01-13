fb2868caf029fe23ad86e057d587206975e3693b
shahzriemg <154407212+shahzriemg@users.noreply.github.com>
2024-01-13 23:04:22 +0800

    Update Employer.java

--- Submission/sec01_perdana/Group1/Source_code/Project/employer/Employer.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/employer/Employer.java
@@ -1,1 +1,16 @@
+package employer;
+import user.User;
+
+public class Employer extends User {
+    private String company;
+
+    public Employer(String username, String password, String company) {
+        super(username, password);
+        this.company = company;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+}
 
