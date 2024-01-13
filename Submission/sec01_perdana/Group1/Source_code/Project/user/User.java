cbc3e48d24fce7b2e4f9df7fd14545a22aae82ee
shahzriemg <154407212+shahzriemg@users.noreply.github.com>
2024-01-13 23:09:30 +0800

    Update User.java

--- Submission/sec01_perdana/Group1/Source_code/Project/user/User.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/user/User.java
@@ -1,1 +1,20 @@
+package user;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
 
