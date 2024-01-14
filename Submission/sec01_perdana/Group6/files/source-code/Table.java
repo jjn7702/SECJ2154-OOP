2f0e68eae8f3945495945139d3358edc2e3438f8
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:02:24 +0800

    update table.java

---
+++ Submission/sec01_perdana/Group6/files/source-code/Table.java
@@ -0,0 +1,24 @@
+public class Table{
+    private int tableNumber;
+    private Order currentOrder;
+
+    public Table(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public Order getCurrentOrder() {
+        return currentOrder;
+    }
+
+    public void placeOrder(Order order) {
+        currentOrder = order;
+    }
+
+    public void clearTable() {
+        currentOrder = null;
+    }
+}
