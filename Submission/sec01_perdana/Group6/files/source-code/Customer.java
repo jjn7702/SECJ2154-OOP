0a91977a30a2e232a75cee664448717c573733ea
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:00:29 +0800

    initial update

---
+++ Submission/sec01_perdana/Group6/files/source-code/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
