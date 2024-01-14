04d1800359d64419832ac0a775672a44590f374a
nabilyhya <nabilyahya39@gmail.com>
2024-01-14 23:17:17 +0800

    update menu.java

--- Submission/sec01_perdana/Group 6/files/source-code/Menu.java
+++ Submission/sec01_perdana/Group 6/files/source-code/Menu.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
+class Menu {
+     private List<FoodItem> items;
 
-public class Menu {
-    
+    public Menu() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(FoodItem item) {
+        items.add(item);
+    }
+
+    public List<FoodItem> getItems() {
+        return new ArrayList<>(items);
+    }
 }
