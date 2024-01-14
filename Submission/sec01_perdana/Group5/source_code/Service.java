5462ee97f8a877ef4c6fb177941ad634d21e49e9
Amirulhani02 <78629605+Amirulhani02@users.noreply.github.com>
2024-01-14 13:23:28 +0800

    Update Service.java

--- Submission/sec01_perdana/Group5/source_code/Service.java
+++ Submission/sec01_perdana/Group5/source_code/Service.java
@@ -1,3 +1,26 @@
+// Service.java
 public class Service {
-    
+    private Customer customer;
+    private Rentable rentable;
+    private int days;
+    private Appointment appointment;
+    private Location pickupLocation;
+
+    public Service(Customer customer, Rentable rentable, int days, Appointment appointment, Location pickupLocation) {
+        this.customer = customer;
+        this.rentable = rentable;
+        this.days = days;
+        this.appointment = appointment;
+        this.pickupLocation = pickupLocation;
+    }
+
+    public double calculateRentalCost() {
+        return rentable.getRentalRate() * days;
+    }
+
+    @Override
+    public String toString() {
+        return customer + " rented " + rentable + " for " + days + " days. Total cost: $" + calculateRentalCost() +
+               "\n" + appointment + "\n" + pickupLocation;
+    }
 }
