c02684bf5a94c0671a7d3155a76e9a444b2cd198
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:00:37 +0800

    initial update

---
+++ Submission/sec01_perdana/Group6/files/source-code/FoodItem.java
@@ -0,0 +1,17 @@
+public class FoodItem {
+    private String name;
+    private double price;
+
+    public FoodItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
