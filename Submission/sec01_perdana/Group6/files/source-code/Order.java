bd9c58a5c97db43df51f4d8df13fecb1655cadad
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:01:23 +0800

    initial commit

---
+++ Submission/sec01_perdana/Group6/files/source-code/Order.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private Customer customer;
+    private List<FoodItem> items;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(FoodItem item, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            items.add(item);
+        }
+    }
+
+    public List<FoodItem> getItems() {
+        return new ArrayList<>(items);
+    }
+
+    public double getTotalPrice() {
+        return items.stream().mapToDouble(FoodItem::getPrice).sum();
+    }
+}
