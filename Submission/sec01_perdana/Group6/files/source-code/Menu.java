ea2e60ae258a54606a1b74c7ccafb504e0d0f3e1
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:00:51 +0800

    fix array

---
+++ Submission/sec01_perdana/Group6/files/source-code/Menu.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private List<FoodItem> items;
+
+    public Menu() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(FoodItem item) {
+        items.add(item);
+    }
+
+    public List<FoodItem> getItems() {
+        return new ArrayList<>(items);
+    }
+}
