d08c9f8590ac6adabcb415f0660b6b66e7ad70e0
IqbalNotAvailable <154432968+IqbalNotAvailable@users.noreply.github.com>
2024-01-14 12:47:37 +0800

    Update JobPortal.java

--- Submission/sec01_perdana/Group1/Source_code/Project/JobPortal.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/JobPortal.java
@@ -104,3 +104,121 @@ public class JobPortal {
       }
     }
       
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Welcome To KerjaMY!");
+    JobPortal jobPortal = new jobPortal();
+    User currentUser = null;
+
+    while (true) {
+      System.out.println("1. Sign Up");
+      System.out.println("2. Sign In");
+      System.out.println("3. Exit");
+      System.out.println("Your option: ");
+      int choice = sc.nextInt();
+      System.out.println("");
+      sc.nextLine();
+
+      if (choice == 1) {
+        //Sign Up
+        System.out.println("Enter username: ");
+        String resume = sc.nextLine();
+        System.out.println("Enter password:");
+        String pass = sc.nextLine();
+
+        System.out.println("\nAre you a Job Seeker or Employer? (1. Job Seeker, 2. Employer)");
+        int userType = sc.nextInt();
+        sc.nextLine();
+
+        if (userType == 1) {
+          System.out.println("Enter your resume: ");
+          String resume = sc.nextLine();
+          JobSeeker jobSeeker = new JobSeeker(username, password, resume);
+          jobPortal.signUp.registerUser(jobSeeker);
+        } else if (userType == 2) {
+            System.out.println("Enter your company: ");
+            String company = sc.nextLine();
+            Employer employer = new Employer(username, password, company);
+            jobPortal.signUp.registerUser(employer);
+        }
+      } else if (choice == 2) {
+          //Sign Up
+          System.out.println("Enter your username: ");
+          String username = sc.nextLine();
+          System.out.println("Enter your password: ");
+          String password = sc.nextLine();
+
+          currentUser = jobPortal.signIn.signIn(username, password);
+          if (currentUser != null) {
+            System.out.println("Logged in successfully");
+            break;
+          }
+      } else if (choice == 3) {
+        //Exit
+        System.out.println("Exiting. Goodbye");
+        System.exit(0);
+      }
+    }
+
+    //Display job after sign in
+    jobPortal.displayJobs();
+
+    while (true) {
+      System.out.println("Main Menu:");
+      System.out.println("1. Apply for a Job");
+      System.out.println("2. Post a Job");
+      System.out.println("3. Exit");
+      System.out.print("Select an option: ");
+      int mainPageChoice = scanner.nextInt();
+      scanner.nextLine();
+
+      if (mainPageChoice == 1) {
+        //Apply job
+        if (curentUser != null && currentUser instanceof JoobSeeker) {
+          jobPortal.displayJobs();
+          System.out.println("Enter the title of the job you want to apply for:");
+          String jobTitle = sc.nextLine();
+
+          JobPosting selectedJob = null;
+          for (JobPosting job : jobPortal.jobPostings) {
+            if (job.getJobTitle().equals(jobTitle)) {
+              selectedJob = job;
+              break;
+            }
+          }
+
+          if (selectedJob != null) {
+            jobPortal.applyForJob((JobSeeker) currentUser, selectedJob);
+            System.out.println("Job application submitted successfully!\n");
+          } else {
+              System.out.println("Job not found!\n");
+          }
+        } else {
+            System.out.println("You must be logged in as a Job Seeker to apply for a job.");
+        }
+      } else if (mainPageChoice == 2) {
+        //Post Job
+        if (currentUser != null && currentUser instanceof Employer) {
+          System.out.println("Enter job title:");
+          String jobTitle = scanner.nextLine();
+          System.out.println("Enter job pay:");
+          String jobPay = scanner.nextLine();
+          System.out.println("Enter job description:");
+          String jobDescription = scanner.nextLine();
+
+          JobPosting newJob = new JobPosting (jobTitle, jobPay, jobDescription);
+          jobPortal.postJob(newJob);
+        } else {
+            System.out.println("You must be logged in as an Employer to post a job.");
+        }
+      } else if (mainPageChoice == 3) {
+        //Exit
+        System.out.println("exiting. Goodbye");
+        break;
+      }
+    }
+    sc.close();
+  }
+}
+    
