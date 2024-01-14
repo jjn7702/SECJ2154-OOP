7fe35c0448c44c0acb3127195730e60f35ef341d
Najwa Asyilah <najwasyilahazid@gmail.com>
2024-01-14 16:45:11 +0800

    update fooditem.java

--- Submission/sec01_perdana/Group 6/files/source-code/FoodItem.java
+++ Submission/sec01_perdana/Group 6/files/source-code/FoodItem.java
@@ -1,4 +1,19 @@
+import java.util.*;
 
-public class FoodItem {
-    
+class FoodItem {
+    private String name;
+    private double price;
+
+    public FoodItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
