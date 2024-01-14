b7d7a189e06b975d20cf9107ad546f7779cd317c
adibfahmy <adibroslan@gmail.com>
2024-01-14 15:51:30 +0800

    update Restaurant.java

--- Submission/sec01_perdana/Group 6/files/source-code/Restaurant.java
+++ Submission/sec01_perdana/Group 6/files/source-code/Restaurant.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
+class Restaurant {
+    private Menu menu;
+    private List<Table> tables;
+
+    public Restaurant() {
+        this.menu = new Menu();
+        this.tables = new ArrayList<>();
+    }
+
+    public void addMenuItem(FoodItem item) {
+        menu.addItem(item);
+    }
 
 
-public class Restaurant {
-    
 }
