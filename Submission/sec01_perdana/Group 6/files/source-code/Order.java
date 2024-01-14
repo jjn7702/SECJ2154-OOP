549bdb764a65f84f513d90a5f9d9f611d3c763d3
nabilyhya <nabilyahya39@gmail.com>
2024-01-14 23:18:00 +0800

    update order.java

--- Submission/sec01_perdana/Group 6/files/source-code/Order.java
+++ Submission/sec01_perdana/Group 6/files/source-code/Order.java
@@ -1,3 +1,25 @@
-public class Order {
-    
+import java.util.*;
+
+class Order {
+    private Customer customer;
+    private List<FoodItem> items;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(FoodItem item, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            items.add(item);
+        }
+    }
+
+    public List<FoodItem> getItems() {
+        return new ArrayList<>(items);
+    }
+
+    public double getTotalPrice() {
+        return items.stream().mapToDouble(FoodItem::getPrice).sum();
+    }
 }
