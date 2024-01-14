98cbb29ac9fa85ee3ea6a068d6b64de1669d543a
Najwa Asyilah <najwasyilahazid@gmail.com>
2024-01-14 16:44:59 +0800

    update customer.java

--- Submission/sec01_perdana/Group 6/files/source-code/Customer.java
+++ Submission/sec01_perdana/Group 6/files/source-code/Customer.java
@@ -1,5 +1,11 @@
+class Customer {
+    private String name;
 
+    public Customer(String name) {
+        this.name = name;
+    }
 
-public class Customer {
-    
+    public String getName() {
+        return name;
+    }
 }
