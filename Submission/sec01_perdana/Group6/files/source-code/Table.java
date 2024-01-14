5a7f03983068c19fdea9abadf2891db1874574a8
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:30:16 +0800

    fix reservation

--- Submission/sec01_perdana/Group6/files/source-code/Table.java
+++ Submission/sec01_perdana/Group6/files/source-code/Table.java
@@ -1,9 +1,11 @@
-public class Table{
+class Table {
     private int tableNumber;
     private Order currentOrder;
+    private boolean isReserved;
 
     public Table(int tableNumber) {
         this.tableNumber = tableNumber;
+        this.isReserved = false;
     }
 
     public int getTableNumber() {
@@ -20,5 +22,17 @@ public class Table{
 
     public void clearTable() {
         currentOrder = null;
+    }
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    public void reserveTable() {
+        isReserved = true;
+    }
+
+    public void cancelReservation() {
+        isReserved = false;
     }
 }
