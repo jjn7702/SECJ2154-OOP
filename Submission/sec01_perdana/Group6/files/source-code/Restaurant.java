cfd42b6da69ba391308884bff179bf5f209d7fbe
adibfahmy <adibroslan@gmail.com>
2024-01-15 00:29:55 +0800

    add reservation function

--- Submission/sec01_perdana/Group6/files/source-code/Restaurant.java
+++ Submission/sec01_perdana/Group6/files/source-code/Restaurant.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Restaurant {
     private Menu menu;
     private List<Table> tables;
+    private List<Reservation> reservations;
 
     public Restaurant() {
         this.menu = new Menu();
         this.tables = new ArrayList<>();
+        this.reservations = new ArrayList<>();
+        initializeTables();
     }
 
     public void addMenuItem(FoodItem item) {
@@ -23,6 +27,30 @@ public class Restaurant {
     }
 
     public List<Table> getTables() {
-        return new ArrayList<>(tables);
+        return tables;
+    }
+
+    public boolean makeReservation(Table table, Date reservationTime) {
+        if (isTableAvailable(table, reservationTime)) {
+            Reservation reservation = new Reservation(table, reservationTime);
+            reservations.add(reservation);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isTableAvailable(Table table, Date reservationTime) {
+        for (Reservation reservation : reservations) {
+            if (reservation.getTable() == table && !reservation.isAvailable(reservationTime)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void initializeTables() {
+        for (int i = 1; i <= 5; i++) {
+            addTable(new Table(i));
+        }
     }
 }
