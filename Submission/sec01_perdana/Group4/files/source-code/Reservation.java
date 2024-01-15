59a72d5e68f8727723d3fefb30607503aa17e2a8
Dennishazmi <110548732+Dennishazmi@users.noreply.github.com>
2024-01-15 22:56:55 +0800

    Update Reservation.java

--- Submission/sec01_perdana/Group4/files/source-code/Reservation.java
+++ Submission/sec01_perdana/Group4/files/source-code/Reservation.java
@@ -1,3 +1,20 @@
-public class Reservation {
-    
+// Reservation class
+class Reservation {
+    private List<Ticket> tickets;
+
+    public Reservation() {
+        this.tickets = new ArrayList<>();
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public void addTicket(Ticket ticket) {
+        this.tickets.add(ticket);
+    }
 }
