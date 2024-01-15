69d3840ac984b5d5e3ab0575d3b770c7fb1f499e
FirdausCader <99279672+FirdausCader@users.noreply.github.com>
2024-01-16 00:11:35 +0800

    Update Booking and Flight

--- Submission/sec01_perdana/Group4/files/source-code/Booking.java
+++ Submission/sec01_perdana/Group4/files/source-code/Booking.java
@@ -1,3 +1,45 @@
 public class Booking {
-    
+    private Passenger passenger;
+    private Flight flight;
+    private Seat seat;
+    private Airline airline;
+
+    public Booking(Passenger passenger, Flight flight, Seat seat, Airline airline) {
+        this.passenger = passenger;
+        this.flight = flight;
+        this.seat = seat;
+        this.airline = airline;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public void setAirline(Airline airline) {
+        this.airline = airline;
+    }
 }
--- Submission/sec01_perdana/Group4/files/source-code/Flight.java
+++ Submission/sec01_perdana/Group4/files/source-code/Flight.java
@@ -1,3 +1,36 @@
 public class Flight extends SkyInfo{
-    
+    private String origin;
+    private String destination;
+    private String boardingTime;
+
+    public Flight(String origin, String destination, String boardingTime) {
+        super("Flight");
+        this.origin = origin;
+        this.destination = destination;
+        this.boardingTime = boardingTime;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getBoardingTime() {
+        return boardingTime;
+    }
+
+    public void setBoardingTime(String boardingTime) {
+        this.boardingTime = boardingTime;
+    }
 }
