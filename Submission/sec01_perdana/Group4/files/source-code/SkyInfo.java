eb8e9ac162e3fafd844cf3d9fee0c1d53646d455
MuhdIzmeer <59471170+ItsJustIzmeer@users.noreply.github.com>
2024-01-15 23:29:46 +0800

    Update SkyInfo.java

--- Submission/sec01_perdana/Group4/files/source-code/SkyInfo.java
+++ Submission/sec01_perdana/Group4/files/source-code/SkyInfo.java
@@ -1,3 +1,15 @@
 abstract class SkyInfo {
-    
+    private String info;
+
+    public SkyInfo(String info) {
+        this.info = info;
+    }
+
+    public String getName() {
+        return info;
+    }
+
+    public void setName(String info) {
+        this.info = info;
+    }
 }
