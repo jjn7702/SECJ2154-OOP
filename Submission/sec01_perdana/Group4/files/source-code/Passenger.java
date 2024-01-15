6a03c0c400c72dd9dcd6bc69abc30e7a7345f10e
FirdausCader <99279672+FirdausCader@users.noreply.github.com>
2024-01-16 00:12:49 +0800

    Update Passenger

--- Submission/sec01_perdana/Group4/files/source-code/Passenger.java
+++ Submission/sec01_perdana/Group4/files/source-code/Passenger.java
@@ -1,3 +1,25 @@
 public class Passenger {
-    
+    private String name;
+    private String contactInfo;
+
+    public Passenger(String name, String contactInfo) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
 }
