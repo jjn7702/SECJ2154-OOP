3b4faf71dd588d08fa5f08944b45ae8a609e0368
IqbalNotAvailable <154432968+IqbalNotAvailable@users.noreply.github.com>
2024-01-16 06:47:44 +0800

    Update JobPortal.java

--- Submission/sec01_perdana/Group1/Source_code/Project/JobPortal.java
+++ Submission/sec01_perdana/Group1/Source_code/Project/JobPortal.java
@@ -30,33 +30,13 @@ public class JobPortal {
         this.signUp = new SignUp(users);
     }
 
-    public void registerUser(User user) {
-        users.add(user);
-        writeUsersToFile();
-        System.out.println("User registered successfully!");
-    }
-
-    public User signIn(String username, String password) {
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                System.out.println("Sign in successful!");
-                return user;
-            }
-        }
-        System.out.println("Invalid username or password. Sign in failed.");
-        return null;
-    }
-
     public void postJob(JobPosting jobPosting) {
-        jobPostings.add(jobPosting);
-        writeJobsToFile();
-        System.out.println("Job posted successfully!");
+        jobPosting.postJob(jobPostings);
     }
 
     public void applyForJob(JobSeeker jobSeeker, JobPosting jobPosting) {
         JobApplication application = new JobApplication(jobSeeker, jobPosting, "Applied");
-        jobApplications.add(application); 
-        System.out.println("Job application submitted successfully!");
+        jobApplications.add(application);
     }
 
     private List<User> readUsersFromFile() {
@@ -79,7 +59,7 @@ public class JobPortal {
                 }
             }
         } catch (IOException e) {
-            System.out.println("Error reading users from file: " + e.getMessage());
+            System.out.println("\n\t\t\t\tError reading users from file: " + e.getMessage());
         }
         return userList;
     }
@@ -94,126 +74,88 @@ public class JobPortal {
                 jobPay = reader.readLine();
                 jobDescription = reader.readLine();
                 jobs.add(new JobPosting(jobTitle, jobPay, jobDescription));
-                
+
                 reader.readLine();
             }
         } catch (IOException e) {
-            System.out.println("Error reading jobs from file: " + e.getMessage());
+            System.out.println("\n\t\t\t\tError reading jobs from file: " + e.getMessage());
         }
         return jobs;
     }
 
-    private void writeUsersToFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_FILE_PATH, true))) {
-            User latestUser = users.get(users.size() - 1);
-            writer.write(latestUser.getUsername());
-            writer.newLine();
-            writer.write(latestUser.getPassword());
-            writer.newLine();
-
-            if (latestUser instanceof JobSeeker) {
-                writer.write("JobSeeker");
-                writer.newLine();
-                writer.write(((JobSeeker) latestUser).getResume());
-                writer.newLine();
-            } else if (latestUser instanceof Employer) {
-                writer.write("Employer");
-                writer.newLine();
-                writer.write(((Employer) latestUser).getCompany());
-                writer.newLine();
-            }
-
-            writer.newLine(); 
-        } catch (IOException e) {
-            System.out.println("Error writing user to file: " + e.getMessage());
-        }
-    }
-
-    private void writeJobsToFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(JOB_FILE_PATH, true))) {
-            JobPosting latestJob = jobPostings.get(jobPostings.size() - 1);
-            writer.write(latestJob.getJobTitle());
-            writer.newLine();
-            writer.write(latestJob.getJobPay());
-            writer.newLine();
-            writer.write(latestJob.getJobDescription());
-            writer.newLine();
-        } catch (IOException e) {
-            System.out.println("Error writing job to file: " + e.getMessage());
-        }
-    }
-
     public void displayJobs() {
-        System.out.println("\nAvailable Jobs:\n");
+        System.out.println("\n\t\t\t\t   +================== AVAILABLE JOBS ===================+");
         for (JobPosting job : jobPostings) {
-            System.out.printf("Title: %s%n", job.getJobTitle());
-            System.out.printf("Pay: %s%n", job.getJobPay());
-            System.out.printf("Description: %s%n", job.getJobDescription());
-            System.out.println("--------------\n");
+            System.out.printf("\t\t\t\t   | Title: %-40s \n", job.getJobTitle());
+            System.out.printf("\t\t\t\t   | Pay: %-42s \n", job.getJobPay());
+            System.out.printf("\t\t\t\t   | Description: %-36s \n", job.getJobDescription());
+            System.out.println("\t\t\t\t   +-------------------------------------------------------+");
         }
     }
 
     public void displayJobApplications() {
-        System.out.println("\nJob Applications:\n");
+        System.out.println("\n\t\t\t\t   +================== JOB APPLICATIONS ===================+");
         for (JobApplication application : jobApplications) {
-            System.out.printf("Applicant: %s%n", application.getApplicant().getUsername());
-            System.out.printf("Job Title: %s%n", application.getJob().getJobTitle());
-            System.out.printf("Status: %s%n", application.getStatus());
-            System.out.println("--------------\n");
+            System.out.printf("\t\t\t\t   | Applicant: %-35s \n", application.getApplicant().getUsername());
+            System.out.printf("\t\t\t\t   | Job Title: %-36s \n", application.getJob().getJobTitle());
+            System.out.printf("\t\t\t\t   | Status: %-38s \n", application.getStatus());
+            System.out.println("\t\t\t\t   +--------------------------------------------------------+");
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Job Portal System!");
+        System.out.println("\t\t\t\t   Welcome to the Job Portal System!");
         JobPortal jobPortal = new JobPortal();
         User currentUser = null;
 
         while (true) {
-            System.out.println("1. Sign Up");
-            System.out.println("2. Sign In");
-            System.out.println("3. Exit");
-            System.out.print("Select an option: ");
+            System.out.println("\n\t\t\t\t   +================== JOB PORTAL ====================+");
+            System.out.println("\t\t\t\t   +1. \t\tSign Up" + "\t\t\t\t      +");
+            System.out.println("\t\t\t\t   +2. \t\tSign In" + "\t\t\t\t      +");
+            System.out.println("\t\t\t\t   +3. \t\tExit" + "\t\t\t\t      +");
+            System.out.println("\t\t\t\t   +==================================================+");
+            System.out.print("\n\n\t\t\t\t\t\tEnter your choice: ");
             int choice = scanner.nextInt();
             System.out.println("");
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             if (choice == 1) {
-                System.out.println("Enter your username:");
+                System.out.print("\t\t\t\t\t\tEnter your username: ");
                 String username = scanner.nextLine();
-                System.out.println("Enter your password:");
+                System.out.print("\n\t\t\t\t\t\tEnter your password: ");
                 String password = scanner.nextLine();
 
-                System.out.println("\nAre you a Job Seeker or Employer? (1. Job Seeker, 2. Employer)");
+                System.out.print("\n\t\t\t\t\t\tAre you a Job Seeker or Employer? (1. Job Seeker, 2. Employer): ");
                 int userType = scanner.nextInt();
-                scanner.nextLine(); 
+                scanner.nextLine();
 
                 if (userType == 1) {
-                    System.out.println("Enter your resume:");
+                    System.out.print("\n\t\t\t\t\t\tEnter your resume: ");
                     String resume = scanner.nextLine();
                     JobSeeker jobSeeker = new JobSeeker(username, password, resume);
                     jobPortal.signUp.registerUser(jobSeeker);
                 } else if (userType == 2) {
-                    System.out.println("Enter your company name:");
+                    System.out.print("\n\t\t\t\t\t\tEnter your company name: ");
                     String company = scanner.nextLine();
                     Employer employer = new Employer(username, password, company);
                     jobPortal.signUp.registerUser(employer);
                 }
             } else if (choice == 2) {
-                System.out.println("Enter your username:");
+                System.out.print("\t\t\t\t\t\tEnter your username: ");
                 String username = scanner.nextLine();
-                System.out.println("Enter your password:");
+                System.out.print("\n\t\t\t\t\t\tEnter your password: ");
                 String password = scanner.nextLine();
 
                 currentUser = jobPortal.signIn.signIn(username, password);
                 if (currentUser != null) {
-                    System.out.println("Logged in successfully!");
+                    System.out.println("\n\n\t\t\t\t\t\tLogged in successfully!");
                     break;
                 }
             } else if (choice == 3) {
-                
-                System.out.println("Exiting. Goodbye!");
+
+                System.out.println("\n\n\t\t\t\t\t\tExiting. Goodbye!");
                 System.exit(0);
             }
         }
@@ -221,20 +163,21 @@ public class JobPortal {
         jobPortal.displayJobs();
 
         while (true) {
-            System.out.println("Main Menu:");
-            System.out.println("1. Apply for a Job");
-            System.out.println("2. Post a Job");
-            System.out.println("3. Display Job Applications");
-            System.out.println("4. Exit");
-            System.out.print("Select an option: ");
+            System.out.println("\n\t\t\t\t   +================== MAIN MENU =====================+");
+            System.out.println("\t\t\t\t   1. \t\tApply for a Job");
+            System.out.println("\t\t\t\t   2. \t\tPost a Job");
+            System.out.println("\t\t\t\t   3. \t\tDisplay Job Applications");
+            System.out.println("\t\t\t\t   4. \t\tExit");
+            System.out.println("\t\t\t\t   +==================================================+");
+            System.out.print("\n\n\t\t\t\t\t\tEnter your choice: ");
             int mainPageChoice = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             if (mainPageChoice == 1) {
-                
+
                 if (currentUser != null && currentUser instanceof JobSeeker) {
                     jobPortal.displayJobs();
-                    System.out.println("Enter the title of the job you want to apply for:");
+                    System.out.print("\n\n\t\t\t\t\t\tEnter the title of the job you want to apply for: ");
                     String jobTitle = scanner.nextLine();
 
                     JobPosting selectedJob = null;
@@ -247,34 +190,34 @@ public class JobPortal {
 
                     if (selectedJob != null) {
                         jobPortal.applyForJob((JobSeeker) currentUser, selectedJob);
-                        System.out.println("Job application submitted successfully!\n");
+                        System.out.println("\n\n\t\t\t\t\t\tJob application submitted successfully!\n");
                     } else {
-                        System.out.println("Job not found!\n");
+                        System.out.println("\n\n\t\t\t\t\t\tJob not found!\n");
                     }
                 } else {
-                    System.out.println("You must be logged in as a Job Seeker to apply for a job.");
+                    System.out.println("\n\n\t\t\t\t\t\tYou must be logged in as a Job Seeker to apply for a job.");
                 }
             } else if (mainPageChoice == 2) {
-                
+
                 if (currentUser != null && currentUser instanceof Employer) {
-                    System.out.println("Enter job title:");
+                    System.out.print("\t\t\t\t\t\tEnter job title: ");
                     String jobTitle = scanner.nextLine();
-                    System.out.println("Enter job pay:");
+                    System.out.print("\t\t\t\t\t\tEnter job pay: ");
                     String jobPay = scanner.nextLine();
-                    System.out.println("Enter job description:");
+                    System.out.print("\t\t\t\t\t\tEnter job description: ");
                     String jobDescription = scanner.nextLine();
 
                     JobPosting newJob = new JobPosting(jobTitle, jobPay, jobDescription);
                     jobPortal.postJob(newJob);
                 } else {
-                    System.out.println("You must be logged in as an Employer to post a job.");
+                    System.out.println("\n\n\t\t\t\t\t\tYou must be logged in as an Employer to post a job.");
                 }
+
             } else if (mainPageChoice == 3) {
-                
                 jobPortal.displayJobApplications();
+
             } else if (mainPageChoice == 4) {
-                
-                System.out.println("Exiting. Goodbye!");
+                System.out.println("\n\n\t\t\t\t\t\tExiting. Goodbye!");
                 break;
             }
         }
@@ -282,3 +225,5 @@ public class JobPortal {
         scanner.close();
     }
 }
+
+
