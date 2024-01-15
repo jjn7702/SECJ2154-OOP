2323a12f174dead119e6c4e9b069f87ef7e24748
IswaryShan <154406154+IswaryShan@users.noreply.github.com>
2024-01-16 01:43:00 +0800

    Update Location.java

--- Submission/sec01_perdana/Group5/source_code/Location.java
+++ Submission/sec01_perdana/Group5/source_code/Location.java
@@ -1,11 +1,43 @@
 public class Location {
-    private String locationPickup;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
 
-    public Location(String locationPickup) {
-        this.locationPickup = locationPickup;
+    public Location(String address, String city, String state, String zipCode) {
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
     }
 
+    // Getter methods
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    // Override toString() to provide a meaningful representation of the object
+    @Override
     public String toString() {
-        return "Rental Location: " + locationPickup;
+        return "Location{" +
+                "address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
     }
 }
+
+
