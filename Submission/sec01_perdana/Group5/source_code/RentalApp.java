87242e8d537e71c0be0797ec2b92621b80726142
Amirulhani02 <amirulhani02@gmail.com>
2024-01-15 18:56:41 +0800

    update rental app and system

--- Submission/sec01_perdana/Group5/source_code/RentalApp.java
+++ Submission/sec01_perdana/Group5/source_code/RentalApp.java
@@ -1,4 +1,11 @@
-/
+import java.util.Scanner;
+
 public class RentalApp {
- 
+    public static void main(String[] args) {
+        RentalSystem rentalSystem = new RentalSystem();
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
 }
--- Submission/sec01_perdana/Group5/source_code/RentalSystem.java
+++ Submission/sec01_perdana/Group5/source_code/RentalSystem.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class RentalSystem {
-    
+    private List<Rental> rentals;
+
+    public RentalSystem() {
+        this.rentals = new ArrayList<>();
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+
+    public static void main(String[] args) {
+    }
 }
