0b81a2d0f83cc41264e908a13373ace08fc8ebcc
IswaryShan <154406154+IswaryShan@users.noreply.github.com>
2024-01-15 17:23:22 +0800

    Update Reservation.java

--- Submission/sec01_perdana/Group5/source_code/Reservation.java
+++ Submission/sec01_perdana/Group5/source_code/Reservation.java
@@ -1,19 +1,19 @@
 public class Reservation {
     private Customer customer;
     private Rentable reservedVehicle;
-    private Location pickupLocation;
+    private Location locationPickup;
     private Date reservationDate;
 
-    public Reservation(Customer customer, Rentable reservedVehicle, Location pickupLocation, Date reservationDate) {
+    public Reservation(Customer customer, Rentable reservedVehicle, Location locationPickup, Date reservationDate) {
         this.customer = customer;
         this.reservedVehicle = reservedVehicle;
-        this.pickupLocation = pickupLocation;
+        this.locationPickup = locationPickup;
         this.reservationDate = reservationDate;
     }
 
     public String toString() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return customer + " reserved " + reservedVehicle + " at " + sdf.format(reservationDate) + "\n" + locationpickup;
+        return customer + " reserved " + reservedVehicle + " at " + sdf.format(reservationDate) + "\n" + locationPickup;
     }
 }
 
