1f957dc64a4191eb6a24d4650e3dd1d218ef98da
Danesh Muthu Krisnan <37767781+DaneshIV@users.noreply.github.com>
2024-01-16 13:56:26 +0000

    Update ReturnBook class

---
+++ Submission/sec01_perdana/Group 8/src/Library/ReturnBook.java
@@ -0,0 +1,32 @@
+package Library;
+import java.util.Scanner;
+public class ReturnBook implements IOOperation {
+
+    Scanner scanner = new Scanner(System.in);
+    public void oper(Database database,User user){
+        System.out.println("Enter book name : ");
+        String bookName = scanner.nextLine();
+
+        if(!database.getBrws().isEmpty()){
+            for(Borrowing b : database.getBrws()){
+                if(b.getBook().getTitle().matches(bookName) && b.getUser().getName().matches(user.getName())){
+                    Book book = b.getBook();
+                    int i = database.getAllBooks().indexOf(book);
+                    if(b.getDaysleft()>0){
+                        System.out.println("You are late " + "You have to pay" + Math.abs(b.getDaysleft()*50) + "as fine");
+                    }
+                    book.setBrwCoppies(book.getBrwCoppies()+1);
+                    database.returnBook(b, book, i);
+                    System.out.println("Book returned successfully\n");
+                    break;
+                }else{
+                    System.out.println("You didn't borrow this book\n");
+                }
+            }
+        }else{
+            System.out.println("You didn't borrow any book\n");
+        }
+        user.menu(database, user);
+
+    }
+}
