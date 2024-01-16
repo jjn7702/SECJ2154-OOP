85c9c705ec217aca01f9a66cc21f27252f3be826
Mnqarlz <aiman020404@gmail.com>
2024-01-16 21:48:03 +0800

    update staff class

---
+++ Submission/sec01_perdana/Group7/source-code/HRMS/src/Staff.java
@@ -0,0 +1,23 @@
+class Staff extends Employee {
+    private Position position;
+
+    public Staff(int employeeID, String firstName, String lastName, String dateOfBirth, String gender,
+            String contactInfo, String email, Position position) {
+        super(employeeID, firstName, lastName, dateOfBirth, gender, contactInfo, email);
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void displayEmployeeInfo() {
+        System.out.println("Staff Member Information:");
+        super.displayEmployeeInfo();
+        System.out.println("Position: " + position.getTitle());
+    }
+}
