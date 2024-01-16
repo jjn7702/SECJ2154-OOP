ac1c26351166d3f2398ed894362a20f34762f17f
FirdausCader <99279672+FirdausCader@users.noreply.github.com>
2024-01-16 10:07:02 +0800

    update reservation and SkyReserve

--- Submission/sec01_perdana/Group4/files/source-code/Reservation.java
+++ Submission/sec01_perdana/Group4/files/source-code/Reservation.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Reservation {
     private List<Ticket> tickets;
 
--- Submission/sec01_perdana/Group4/files/source-code/SkyReserve.java
+++ Submission/sec01_perdana/Group4/files/source-code/SkyReserve.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class SkyReserve{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
