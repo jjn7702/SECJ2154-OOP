c63cb8277ed0d36274c2afed532860e2ee6d1010
Anushka Roshni Sivakumar <82142164+anushkagemini@users.noreply.github.com>
2024-01-16 12:53:35 +0000

    added user class

---
+++ Submission/sec01_perdana/Group 8/src/Library/User.java
@@ -0,0 +1,44 @@
+package Library;
+public abstract class User{
+    protected String name;
+    protected String email;
+    protected String phoneNumber;
+    protected IOOperation[] operations;
+
+    public User(){};
+    
+    public User(String name){
+        this.name = name;
+    }
+
+    public User(String name, String email, String phoneNumber){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    abstract  public String toString();
+
+    abstract public void menu(Database database,User user);
+
+}
