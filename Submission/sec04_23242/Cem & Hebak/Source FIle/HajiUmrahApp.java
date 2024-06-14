ec202b9b5c070eb3398fc7bbb94cee19fb9314a9
Alif Aiman <147372857+aalfmnsrr@users.noreply.github.com>
2024-06-14 16:25:13 +0000

    create separate file

--- Submission/sec04_23242/Cem & Hebak/Source FIle/HajiUmrahApp.java
+++ Submission/sec04_23242/Cem & Hebak/Source FIle/HajiUmrahApp.java
@@ -369,25 +369,6 @@ enum Pelancongan_package {
 
 }
 
-class Kursus{
-    private String kursusID;
-    private String time;
-    private String date;
-    private String kursusPlace;
-    private String nameUstaz;
-
-    public Kursus(String kursusID, String time, String date, String kursusPlace, String nameUstaz){
-        this.kursusID = kursusID;
-        this.time = time;
-        this.date = date;
-        this.kursusPlace = kursusPlace;
-        this.nameUstaz = nameUstaz;
-    }
-
-    public void Display_Kursus_info(){
-        System.out.printf("%-5s %-8s %-12s %-30s %-15s\n", kursusID, time, date, kursusPlace, nameUstaz);
-    }
-}
 
 public class HajiUmrahApp {
     public static void main(String[] args) {
