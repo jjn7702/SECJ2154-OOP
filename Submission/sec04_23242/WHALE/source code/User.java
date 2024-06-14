9e6d7b8ee7bf6a45b45bbf0e3a16883bd45cfd87
AffifFarhan <muhammadaffiffarhan@graduate.utm.my>
2024-06-14 14:05:07 +0800

    Update User.java

--- Submission/sec04_23242/WHALE/source code/User.java
+++ Submission/sec04_23242/WHALE/source code/User.java
@@ -1,11 +1,17 @@
 public class User {
+    // protected and private attributes
     protected Address add;
     protected String fName;
     protected String lName;
     private int age;
-    private string email;
-    // private Address address;
+    private String email;
 
-    public void setName(String name) {
+    // public attributes
+    public User(String fn, String ln, int a, String em, Address add) {
+        fName = fn;
+        lName = ln;
+        age = a;
+        email = em;
+        this.add = add;
     }
 }
