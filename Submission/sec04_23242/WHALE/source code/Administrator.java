5655c2dde84cd20e3fba1c10b1cf7ba84f1cb1f9
AffifFarhan <muhammadaffiffarhan@graduate.utm.my>
2024-06-14 14:18:31 +0800

    Update Administrator.java

--- Submission/sec04_23242/WHALE/source code/Administrator.java
+++ Submission/sec04_23242/WHALE/source code/Administrator.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Administrator extends User {
+    // private attributes
+    public String adminId;
+    private String job;
+    private ArrayList stud;
+    private Scholarship scholar;
+    private Apply application;
 
 }
