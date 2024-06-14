44fe443396c6a3982568c163f3ca8e214187ec17
AffifFarhan <muhammadaffiffarhan@graduate.utm.my>
2024-06-14 14:29:47 +0800

    Update Administrator.java

--- Submission/sec04_23242/WHALE/source code/Administrator.java
+++ Submission/sec04_23242/WHALE/source code/Administrator.java
@@ -1,19 +1,46 @@
 import java.util.ArrayList;
 
 public class Administrator extends User {
-    // private attributes
-    public String adminId;
+    // Private attributes
+    public String adminID;
     private String job;
-    private ArrayList stud;
+    private ArrayList<Student> stud;
     private Scholarship scholar;
     private Apply application;
 
-    //public Attributes
-    public Administrator(String fn, String ln, int a, String em, Address ad){
+    // Public constructor
+    public Administrator(String fn, String ln, int a, String em, Address add, String adminId, String job,
+            ArrayList<Student> stud, Scholarship scholar, Apply application) {
         super(fn, ln, a, em, add);
-        this.adminId = adminId;
+        this.adminID = adminId;
         this.job = job;
         this.stud = stud;
-        
+        this.scholar = scholar;
+        this.application = application;
+    }
+
+    public void setAdminId(String aid) {
+        adminID = aid;
+    }
+
+    public void setJob(String j) {
+        job = j;
+    }
+
+    public String getAdminID() {
+        return adminID;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void evaluateStudent(Student st) {
+        stud.add(st);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Admin ID: " + adminID + " Job: " + job;
     }
 }
