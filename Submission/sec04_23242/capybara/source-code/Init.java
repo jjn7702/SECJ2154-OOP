2068475b435c849c5a81f529a6e29ea2a4129d28
derf <danialerfanshah@gmail.com>
2024-06-14 22:19:52 +0800

    add date and time in Init

--- Submission/sec04_23242/capybara/source-code/Init.java
+++ Submission/sec04_23242/capybara/source-code/Init.java
@@ -41,36 +41,90 @@ public class Init {
 
         JTextField nameField = addTextField("Name Trip", 30, 100, 150, 30);
         JTextField locationField = addTextField("Location Trip", 230, 100, 150, 30);
-        JComboBox<String> transportTypeComboBox = addComboBox("Transport Type", 30, 260, 150, 30, new String[]{"Car", "Bus", "Train", "Miscellaneous"});
-        JTextField transportCostField = addTextField("Transport Cost", 230, 260, 150, 30);
 
-        addButton("Next", 270, 370, e -> showAddActivityScreen(nameField, locationField, transportTypeComboBox, transportCostField));
+        JComboBox<String> transportTypeComboBox = addComboBox("Transport Type", 30, 220, 150, 30,
+                new String[] { "Car", "Bus", "Train", "Miscellaneous" });
+        JTextField transportCostField = addTextField("Transport Cost", 230, 220, 150, 30);
+
+        addButton("Next", 270, 370,
+                e -> showAddActivityScreen(nameField, locationField, transportTypeComboBox, transportCostField));
 
         frame.revalidate();
         frame.repaint();
     }
 
-    private static void showAddActivityScreen(JTextField nameField, JTextField locationField, JComboBox<String> transportTypeComboBox, JTextField transportCostField) {
+    private static void showAddActivityScreen(JTextField nameField, JTextField locationField,
+            JComboBox<String> transportTypeComboBox, JTextField transportCostField) {
         frame.getContentPane().removeAll();
 
         addLabel("Add New Trip | Activity", 30, 1, 400, 100, headerFont);
 
         JTextField activityNameField = addTextField("Activity Name", 30, 80, 150, 30);
         JTextField activityCostField = addTextField("Activity Cost", 30, 200, 150, 30);
-        JComboBox<ColoredItem> colorComboBox = addColorComboBox("Activity Color", 230, 80, 150, 30, new ColoredItem[]{
+        JComboBox<ColoredItem> colorComboBox = addColorComboBox("Activity Color", 230, 80, 150, 30, new ColoredItem[] {
                 new ColoredItem("Family", Color.RED),
                 new ColoredItem("Friend", Color.GREEN),
                 new ColoredItem("Partner", Color.PINK),
                 new ColoredItem("Others", Color.BLUE)
         });
 
-        addButton("Save Trip", 270, 370, e -> saveTrip(nameField, locationField, transportTypeComboBox, transportCostField, activityNameField, activityCostField, colorComboBox));
+        addButton("Next", 270, 370, e -> showAddDateTimeScreen(nameField, locationField, transportTypeComboBox,
+                transportCostField, activityNameField, activityCostField, colorComboBox));
 
         frame.revalidate();
         frame.repaint();
     }
 
-    private static void saveTrip(JTextField nameField, JTextField locationField, JComboBox<String> transportTypeComboBox, JTextField transportCostField, JTextField activityNameField, JTextField activityCostField, JComboBox<ColoredItem> colorComboBox) {
+    private static void showAddDateTimeScreen(JTextField nameField, JTextField locationField,
+            JComboBox<String> transportTypeComboBox, JTextField transportCostField, JTextField activityNameField,
+            JTextField activityCostField, JComboBox<ColoredItem> colorComboBox) {
+        frame.getContentPane().removeAll();
+
+        addLabel("Add New Trip | Date & Time", 30, 1, 400, 100, headerFont);
+
+        // Input fields untuk date and time
+        JPanel datePanel = new JPanel(new FlowLayout());
+        datePanel.add(new JLabel("YYYY:"));
+        JTextField yearField = new JTextField(4);
+        datePanel.add(yearField);
+
+        datePanel.add(new JLabel("MM:"));
+        JTextField monthField = new JTextField(2);
+        datePanel.add(monthField);
+
+        datePanel.add(new JLabel("DD:"));
+        JTextField dayField = new JTextField(2);
+        datePanel.add(dayField);
+
+        datePanel.setBounds(30, 140, 350, 30);
+        frame.add(datePanel);
+
+        JPanel timePanel = new JPanel(new FlowLayout());
+        timePanel.add(new JLabel("HH:"));
+        JTextField hourField = new JTextField(2);
+        timePanel.add(hourField);
+
+        timePanel.add(new JLabel("MM:"));
+        JTextField minuteField = new JTextField(2);
+        timePanel.add(minuteField);
+
+        timePanel.setBounds(30, 180, 350, 30);
+        frame.add(timePanel);
+
+        addButton("Save Trip", 270, 370,
+                e -> saveTrip(nameField, locationField, yearField, monthField, dayField, hourField, minuteField,
+                        transportTypeComboBox, transportCostField, activityNameField, activityCostField,
+                        colorComboBox));
+
+        frame.revalidate();
+        frame.repaint();
+    }
+
+    // Integrated date and time input validation and handling
+    private static void saveTrip(JTextField nameField, JTextField locationField, JTextField yearField,
+            JTextField monthField, JTextField dayField, JTextField hourField, JTextField minuteField,
+            JComboBox<String> transportTypeComboBox, JTextField transportCostField, JTextField activityNameField,
+            JTextField activityCostField, JComboBox<ColoredItem> colorComboBox) {
         Vector<Budget> tripBudgets = new Vector<>();
 
         String transportType = (String) transportTypeComboBox.getSelectedItem();
@@ -82,23 +136,33 @@ public class Init {
         double activityCost = Double.parseDouble(activityCostField.getText());
         Color activityColor = ((ColoredItem) colorComboBox.getSelectedItem()).getColor();
 
-        Transportation transport = new Transportation(transportType, transportCost);
-        Activities activities = new Activities(activityNameField.getText(), activityCost, activityColor);
+        String date = yearField.getText() + "-" + monthField.getText() + "-" + dayField.getText();
+        String time = hourField.getText() + ":" + minuteField.getText();
 
-        tripBudgets.add(transport);
-        tripBudgets.add(activities);
+        // Validate date & time
+        if (DateAndTime.isValidDate(date) && DateAndTime.isValidTime(time)) {
+            DateAndTime dateTime = new DateAndTime(date, time);
+            Transportation transport = new Transportation(transportType, transportCost);
+            Activities activities = new Activities(activityNameField.getText(), activityCost, activityColor);
 
-        campingTrips.add(new CampingTrips(nameField.getText(), locationField.getText(), tripBudgets));
-        showSavedTripDetail(transport, activities);
+            tripBudgets.add(transport);
+            tripBudgets.add(activities);
+
+            campingTrips.add(new CampingTrips(nameField.getText(), locationField.getText(), dateTime, tripBudgets));
+            showSavedTripDetail(transport, activities, dateTime);
+        } else {
+            JOptionPane.showMessageDialog(null, "Invalid date or time format. Please try again.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
 
-    private static void showSavedTripDetail(Transportation transport, Activities activities) {
+    // Display date & time in saved trip details
+    private static void showSavedTripDetail(Transportation transport, Activities activities, DateAndTime dateTime) {
         double totalCost = transport.getCost() + activities.getCost();
         String totalBudgetStr = String.format("Total Budget: RM" + totalCost);
         frame.getContentPane().removeAll();
         addLabel("Saved Trip Detail", 30, 1, 200, 100, headerFont);
 
-      
         addLabel(totalBudgetStr, 30, 80, 300, 30, textFont);
         addLabel("Transport Type: " + transport.getName(), 30, 120, 300, 30, textFont);
         addLabel("Transport Cost: RM" + transport.getCost(), 30, 160, 300, 30, textFont);
@@ -106,16 +170,21 @@ public class Init {
         addLabel("Activity Cost: RM" + activities.getCost(), 30, 240, 300, 30, textFont);
         addLabel("Activity Color: ", 30, 280, 300, 30, textFont);
 
+        // Added date & time display
+        addLabel("Trip Date: " + dateTime.getDate(), 30, 320, 300, 30, textFont);
+        addLabel("Trip Time: " + dateTime.getTime(), 30, 360, 300, 30, textFont);
+
         JLabel colorDisplay = new JLabel(createColoredDotIcon(activities.getColor()));
         colorDisplay.setBounds(150, 310, 30, 30);
         frame.add(colorDisplay);
 
-        addButton("Back to Main Menu", 30, 350, e -> showMainMenu());
+        addButton("Back to Main Menu", 30, 400, e -> showMainMenu());
 
         frame.revalidate();
         frame.repaint();
     }
 
+    // Display date & time in trip list
     private static void showTrips() {
         frame.getContentPane().removeAll();
 
@@ -134,6 +203,8 @@ public class Init {
         for (CampingTrips trip : campingTrips) {
             tripsText.append("Trip Name: ").append(trip.getName()).append("\n");
             tripsText.append("Location: ").append(trip.getLocation()).append("\n");
+            tripsText.append("Date: ").append(trip.getDateTime().getDate()).append("\n");
+            tripsText.append("Time: ").append(trip.getDateTime().getTime()).append("\n");
 
             for (Budget budget : trip.getBudgets()) {
                 if (budget instanceof Transportation) {
@@ -168,7 +239,8 @@ public class Init {
         return textField;
     }
 
-    private static JComboBox<String> addComboBox(String labelText, int x, int y, int width, int height, String[] options) {
+    private static JComboBox<String> addComboBox(String labelText, int x, int y, int width, int height,
+            String[] options) {
         JLabel label = new JLabel(labelText);
         label.setBounds(x, y, width, height);
         frame.add(label);
@@ -180,7 +252,8 @@ public class Init {
         return comboBox;
     }
 
-    private static JComboBox<ColoredItem> addColorComboBox(String labelText, int x, int y, int width, int height, ColoredItem[] items) {
+    private static JComboBox<ColoredItem> addColorComboBox(String labelText, int x, int y, int width, int height,
+            ColoredItem[] items) {
         JLabel label = new JLabel(labelText);
         label.setBounds(x, y, width, height);
         frame.add(label);
@@ -258,7 +331,8 @@ public class Init {
         }
 
         @Override
-        public Component getListCellRendererComponent(JList<? extends ColoredItem> list, ColoredItem value, int index, boolean isSelected, boolean cellHasFocus) {
+        public Component getListCellRendererComponent(JList<? extends ColoredItem> list, ColoredItem value, int index,
+                boolean isSelected, boolean cellHasFocus) {
             if (isSelected) {
                 setBackground(list.getSelectionBackground());
                 setForeground(list.getSelectionForeground());
