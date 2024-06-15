c6db5b4cc397835da8cd9463bbb4a749625af88b
KuganrajUTM <kuganraj@graduate.utm.my>
2024-06-15 18:05:44 +0800

    class Food

--- Submission/sec04_23242/capybara/source-code/Food.java
+++ Submission/sec04_23242/capybara/source-code/Food.java
@@ -1,3 +1,36 @@
-public class Food {
+public class Food extends Item {
+    private String expirationDate; // expirationDate for Packet Food
+    private String type; //Fruit, Lunch meal, Breakfast, Dinner, Packet food 
+    private boolean isVegetarian;
 
+    public Food (String n, int q, double p, String d, String e, String t){
+        super(n,q,p,d);
+        expirationDate = e;
+        type = t;
+    }
+
+    public String getExpirationDate() {
+        if(type == "Packet Food")
+            return expirationDate;
+        else 
+            return "None";
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public void addQuantity(int amount) {
+        if (amount > 0) {
+            setQuantity(getQuantity() + amount);
+            System.out.println("Added " + amount + " " + getName() + ". Total quantity now: " + getQuantity());
+        } else {
+            System.out.println("Invalid quantity. Please enter a positive number.");
+        }
+    }
 }
+
