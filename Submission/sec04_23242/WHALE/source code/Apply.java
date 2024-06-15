00ac89a165094fdc5c46f34dfc3fe3de62e5e884
Hazim89 <147373109+Hazim89@users.noreply.github.com>
2024-06-15 14:05:45 +0000

    Add Apply class

--- Submission/sec04_23242/WHALE/source code/Apply.java
+++ Submission/sec04_23242/WHALE/source code/Apply.java
@@ -1,3 +1,21 @@
 public class Apply {
+    private int studCount;
+
+    public Apply(int studCount) {
+        this.studCount = studCount;
+    }
+
+    public void incrementStudent() {
+        studCount++;
+    }
+
+    public int getStudCount() {
+        return studCount;
+    }
+
+    public void setStudCount(int studCount) {
+        this.studCount = studCount;
+    }
+}
     
 }
