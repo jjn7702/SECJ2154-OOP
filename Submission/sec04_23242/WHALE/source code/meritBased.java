cb6246f6a645d995ff1fc26ef153cd28ae9978e0
izzuddin0911 <139842477+izzuddin0911@users.noreply.github.com>
2024-06-17 06:22:44 +0000

    Delete Global and Local

---
+++ Submission/sec04_23242/WHALE/source code/meritBased.java
@@ -0,0 +1,23 @@
+class meritBased extends Scholarship{
+    private double allowance ;
+
+    public meritBased(String sid, double all){
+        super(sid) ;
+        allowance = all ;
+    }
+
+    public boolean isEligible(Student st){
+        if (st.getCgpa() >= ScholarshipRequirement.minCGPA)
+            return true ;
+        else
+            return false ;
+    }
+
+    public double getAllowance(){
+        return allowance ;
+    }
+
+    public void display(){
+        
+    }
+}
