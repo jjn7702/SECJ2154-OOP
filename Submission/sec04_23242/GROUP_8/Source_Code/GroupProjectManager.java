34462547061c1cbfe292db585ce28307d9197f4f
benkzy2 <benkzy11332@gmail.com>
2024-06-17 15:44:46 +0800

    Update GroupProjectManager.java

--- Submission/sec04_23242/GROUP_8/Source_Code/GroupProjectManager.java
+++ Submission/sec04_23242/GROUP_8/Source_Code/GroupProjectManager.java
@@ -162,3 +162,36 @@ class Team {
     private String teamName;
     
 }
+
+class Report {
+    private String reportID;
+    private String content;
+    private Project project;
+    
+
+    public Report(String reportID, String content, Project project) {
+        this.reportID = reportID;
+        this.content = content;
+        this.project = project;
+    }
+
+    public String getReportID() {
+        return reportID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setReportID(String reportID) {
+        this.reportID = reportID;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+}
