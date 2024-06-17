ba7db2a46f1908b29b3cbd0d8d269067596c31c3
benkzy2 <benkzy11332@gmail.com>
2024-06-17 15:45:39 +0800

    Update GroupProjectManager.java

--- Submission/sec04_23242/GROUP_8/Source_Code/GroupProjectManager.java
+++ Submission/sec04_23242/GROUP_8/Source_Code/GroupProjectManager.java
@@ -195,3 +195,27 @@ class Report {
         return project;
     }
 }
+
+class Review {
+    private String reviewerName;
+    private String feedback;
+    private Report report;
+
+    public Review(String reviewerName, String feedback, Report report) {
+        this.reviewerName = reviewerName;
+        this.feedback = feedback;
+        this.report = report;
+    }
+
+    public String getReviewName() {
+        return reviewerName;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public Report getReport() {
+        return report;
+    }
+}
