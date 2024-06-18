758a2416bc9d57c52d62fcc8cf9d47952cdb83a9
nurishsyafiqa <166276403+nurishsyafiqa@users.noreply.github.com>
2024-06-19 00:09:51 +0800

    Display category

--- Submission/sec04_23242/Group2/Source_Code/Category.java
+++ Submission/sec04_23242/Group2/Source_Code/Category.java
@@ -1,1 +1,16 @@
+public enum Category {
+    FRUITS("Fruits"), VEGETABLES("Vegetables"), DAIRY("Dairy"), MEAT("Meat"), BAKERY("Bakery"), BEVERAGES("Beverages"),
+    SNACKS("Snacks"), OTHERS("Others");
+
+    private String desc;
+
+    private Category(String s) {
+        desc = s;
+    }
+
+    public String toString() {
+        return desc;
+    }
+
+}
 
