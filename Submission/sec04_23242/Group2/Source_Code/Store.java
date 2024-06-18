d0609fc1b8864feb464ba572b63502613ee6c089
Kughanraj Arunasalam <147372879+Kughanraj@users.noreply.github.com>
2024-06-18 21:40:55 +0800

    create store

--- Submission/sec04_23242/Group2/Source_Code/Store.java
+++ Submission/sec04_23242/Group2/Source_Code/Store.java
@@ -1,1 +1,41 @@
+import java.util.*;
 
+public class Store {
+    private String name;
+    private Address add;
+    private Vector<Product> products;
+
+    public Store(String name, Address add) {
+        this.name = name;
+        this.add = add;
+        products = new Vector<>();
+    }
+
+    public void addItem(String name, Category cat) {
+        products.add(new Product(name, cat));
+    }
+
+    public void deleteItem(Product p) {
+        products.remove(p);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vector<Product> getProducts() {
+        return products;
+    }
+
+    public Address getAdd() {
+        return add;
+    }
+
+    public void setAdd(Address add) {
+        this.add = add;
+    }
+}
