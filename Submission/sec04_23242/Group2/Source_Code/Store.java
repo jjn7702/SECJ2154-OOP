0886f52e09cdd32c49a0367a8eec0ea34d553035
Kughanraj Arunasalam <147372879+Kughanraj@users.noreply.github.com>
2024-06-18 21:43:24 +0800

    displayStore func

--- Submission/sec04_23242/Group2/Source_Code/Store.java
+++ Submission/sec04_23242/Group2/Source_Code/Store.java
@@ -46,4 +46,14 @@ public class Store {
     public Product searchItem(int n) {
         return products.get(n);
     }
+
+        public void displayStore() {
+        int n = 0;
+        System.out.printf("%-5s%-15s%-10s\n", "No.", "Item", "Category");
+        for (Product p : products) {
+            System.out.printf("%d%-4s%-15s%-10s\n", (n + 1), ")", p.getName(), p.getCategory());
+            n++;
+        }
+    }
+
 }
