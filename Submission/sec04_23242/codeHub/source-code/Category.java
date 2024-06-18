41fed0d0b8421c77e94d6902e1a739e71a6bf934
Muhammad Abduh Bin Abdul Ba'ari <abduhmuhammad979@gmail.com>
2024-06-18 17:01:17 +0800

    Create Category.java
    define class Category and its subclasses

---
+++ Submission/sec04_23242/codeHub/source-code/Category.java
@@ -0,0 +1,34 @@
+abstract class Category {
+    private int id;
+    private String name, description;
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
+class ShoppingCategory extends Category {
+    public ShoppingCategory(int id) {
+        super(id, "Shopping", "Shopping Transaction");
+    }
+}
+
+class FoodCategory extends Category {
+    public FoodCategory(int id) {
+        super(id, "Food", "Food Transaction");
+    }
+}
