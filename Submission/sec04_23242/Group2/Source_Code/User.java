6a3d7c4a6e16320a66bbdacbb097ff4f029c0aae
Kughanraj Arunasalam <147372879+Kughanraj@users.noreply.github.com>
2024-06-18 21:44:19 +0800

    create user abstract class

--- Submission/sec04_23242/Group2/Source_Code/User.java
+++ Submission/sec04_23242/Group2/Source_Code/User.java
@@ -1,1 +1,40 @@
+public abstract class User {
+    private String password;
+    private String email;
+    private String name;
 
+    public User() {
+    }
+
+    public User(String email, String pass, String name) {
+        this.password = pass;
+        this.email = email;
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.password = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void display();
+}
