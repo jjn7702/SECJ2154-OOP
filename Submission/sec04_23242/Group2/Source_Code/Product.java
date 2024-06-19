f185c768e847179f1ab3906c9294467db90efdda
syazasyafiah <166273337+syazasyafiah@users.noreply.github.com>
2024-06-19 23:40:54 +0800

    add product

--- Submission/sec04_23242/Group2/Source_Code/Product.java
+++ Submission/sec04_23242/Group2/Source_Code/Product.java
@@ -1,1 +1,33 @@
+public class Product {
+    private String name;
+    private Category category;
 
+    public Product(String name, Category cat) {
+        this.name = name;
+        category = cat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean contains(Product p) {
+        if (name.equals(p.getName()) && (category == p.getCategory())) {
+            return true;
+        } else
+            return false;
+    }
+
+}
