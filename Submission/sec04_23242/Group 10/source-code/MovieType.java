5b9c1c1eb5fdd27c3b8448384ca83b67ec71424d
Farah Syahirah <101051181+farahsyahirah25@users.noreply.github.com>
2024-06-19 23:40:57 +0800

    Add files via upload

---
+++ Submission/sec04_23242/Group 10/source-code/MovieType.java
@@ -0,0 +1,26 @@
+//FARAH SYAHIRAH BINTI NOOR JA'AFAR (A23CS3008)
+
+package farah;
+
+import java.util.Vector;
+
+public class MovieType {
+    private String type;
+    private Vector<Movie> movies = new Vector<Movie>();
+
+    public MovieType(String type){
+        this.type = type;
+    }
+
+    public void addMovie(Movie movie){
+        movies.add(movie);
+    }
+
+    public Vector<Movie> getMovies(){
+        return movies;
+    }
+
+    public String getType(){
+        return type;
+    }
+}
