2e5a81951d91b959c3bf5447ae4ca583e0d4ae52
Vennise <165978886+Vennise23@users.noreply.github.com>
2024-06-19 14:21:13 +0800

    Add files via upload

---
+++ Submission/sec04_23242/Group 10/source-code/MovieRecommendationSystem.java
@@ -0,0 +1,84 @@
+package project;
+/*
+ * Name: Vennise Ngoh Yan Leng
+ * Matric ID: B23CS8032
+ */
+
+import java.util.Vector;
+
+public class MovieRecommendationSystem {
+    public static void main(String[] args) {
+        //Generate Data
+        Vector<MovieType> mT = new Vector<MovieType>();
+        Vector<Director> dr = new Vector<Director>();
+        Vector<Movie> mv = new Vector<Movie>();
+
+        String[] mTString = { "Action", "Adventure", "Comedy", "Drama", "Horror", "Sci-Fi", "Fantasy", "Romance",
+                "Thriller", "Mystery", "Crime", "Western", "Musical", "Animation", "Documentary", "Biographical",
+                "Family", "War", "Historical", "Sport" };
+        String[] drString = { "Steven Spielberg", "Martin Scorsese", "Christopher Nolan", "Quentin Tarentino",
+                "Kathryn Bigelow" };
+        int[] drInt = { 53, 57, 26, 32, 43 };
+        String[][] mvString = {
+                { "Jaws", "E.T. the Extra-Terrestrial", "Jyrassuc Park", "Schundler's List", "Saving Private Ryan" },
+                { "Taxi Driver", "Raging Bull", "Goodfellas", "The Departed", "The Irishman" },
+                { "Momento", "The Dark Knight", "Inception", "Interstellar", "Dunkirk" },
+                { "Reservoir Dogs", "Pulp Fiction", "Kill Bill:Volume 1", "Inglourious Basterds",
+                        "Once Upon a Time in Hollywood" },
+                { "Near Dark", "Point Break", "The Hurt Locker", "Zero Dark Thirty", "Detroit" } };
+        for (String m : mTString) {
+            mT.add(new MovieType(m));
+        }
+        for (int i = 0; i < drString.length; i++) {
+            dr.add(new Director(drString[i], drInt[i]));
+        }
+        for (int i = 0; i < mvString.length; i++) {
+            for (int j = 0; j < mvString[i].length; j++) {
+                Vector<MovieType> mvt = new Vector<MovieType>();
+                mvt.add(mT.get((int) (Math.random() * (20))));
+                mvt.add(mT.get((int) (Math.random() * (20))));
+                mvt.add(mT.get((int) (Math.random() * (20))));
+                mvt.add(mT.get((int) (Math.random() * (20))));
+                mvt.add(mT.get((int) (Math.random() * (20))));
+                mvt.add(mT.get((int) (Math.random() * (20))));
+                mv.add(new Movie("M" + i + j, mvString[i][j], dr.get(i), mvt));
+            }
+        }
+
+        User u1 = new PremiumUser("U01", "Vennise", 17.50);
+        u1.watched(mv.get(0));
+        u1.watched(mv.get(1));
+        u1.watched(mv.get(1));
+        u1.watched(mv.get(2));
+        u1.watched(mv.get(2));
+        u1.watched(mv.get(2));
+        u1.watched(mv.get(7));
+        u1.watched(mv.get(8));
+        u1.watched(mv.get(11));
+        u1.watched(mv.get(12));
+        u1.watched(mv.get(11));
+        u1.watched(mv.get(13));
+        u1.watched(mv.get(14));
+        u1.watched(mv.get(15));
+        u1.watched(mv.get(15));
+        u1.watched(mv.get(20));
+
+        u1.addRating("Regular Rating", mv.get(0), 4, "Good");
+        u1.addRating("RegularRating", mv.get(1), 3, "Good");
+        u1.addRating("AnonymousRating", mv.get(2), 1, "Good");
+        u1.addRating("AnonymousRating", mv.get(7), 3, "Good");
+        u1.addRating("AnonymousRating", mv.get(8), 2, "Good");
+        u1.addRating("RegularRating", mv.get(12), 4, "Good");
+        u1.addRating("RegularRating", mv.get(11), 4, "Good");
+        u1.addRating("AnonymousRating", mv.get(14), 1, "Good");
+        u1.addRating("AnonymousRating", mv.get(15), 3, "Good");
+        u1.addRating("AnonymousRating", mv.get(15), 2, "Good");
+        u1.addRating("RegularRating", mv.get(8), 4, "Good");
+        u1.addRating("RegularRating", mv.get(11), 4, "Good");
+
+        RecommendationEngine re1 = new RecommendationEngine(u1);
+        u1.displayInfo();
+        re1.displayRecByFavDirect();
+        re1.displayMostRatedMovies();
+    }
+}
---
+++ Submission/sec04_23242/Group 10/source-code/RecommendationEngine.java
@@ -0,0 +1,129 @@
+package project;
+/*
+ * Name: Vennise Ngoh Yan Leng
+ * Matric ID: B23CS8032
+ */
+
+import java.util.Vector;
+
+public class RecommendationEngine {
+    private User user;
+
+    public RecommendationEngine(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void displayMostRatedMovies() {
+        // get the 3 highest rating, and their Movie Types
+        int[] highestRating = new int[3];
+        Vector<Movie> highestRatedMovies = new Vector<Movie>();
+
+        for (Rating r : user.getRating()) {
+            if (r.getRating() > highestRating[0]) {
+                highestRating[0] = r.getRating();
+                if (highestRatedMovies.isEmpty()) {
+                    highestRatedMovies.add(r.getMovie());
+                } else {
+                    highestRatedMovies.set(0, r.getMovie());
+                }
+            } else if (r.getRating() > highestRating[1]) {
+                highestRating[1] = r.getRating();
+                if (highestRatedMovies.size() <= 1) {
+                    highestRatedMovies.add(r.getMovie());
+                } else {
+                    highestRatedMovies.set(1, r.getMovie());
+                }
+            } else if (r.getRating() > highestRating[2]) {
+                highestRating[2] = r.getRating();
+                if (highestRatedMovies.size() <= 2) {
+                    highestRatedMovies.add(r.getMovie());
+                } else {
+                    highestRatedMovies.set(2, r.getMovie());
+                }
+            }
+        }
+
+        // get the highestRatedMovies MovieTypes and find the most repeated MovieType
+        Vector<MovieType> movieTypes = new Vector<MovieType>();
+        Vector<Integer> numOfMovieTypes = new Vector<Integer>();
+        int highestNumber = 0;
+        for (Movie m : highestRatedMovies) {
+            for (MovieType mt : m.getMovieTypes()) {
+                if (!movieTypes.contains(mt)) {
+                    movieTypes.add(mt);
+                    numOfMovieTypes.add(1);
+                    if (highestNumber < 1) {
+                        highestNumber = 1;
+                    }
+                } else {
+                    int index = movieTypes.indexOf(mt);
+                    int totalNum = numOfMovieTypes.get(index) + 1;
+                    numOfMovieTypes.set(index, totalNum);
+                    if (highestNumber < totalNum) {
+                        highestNumber = totalNum;
+                    }
+                }
+            }
+        }
+        int mostRepeatedMovieType_Index = numOfMovieTypes.indexOf(highestNumber);
+        MovieType movieType = movieTypes.get(mostRepeatedMovieType_Index);
+
+        System.out.println("===========================================");
+        System.out.println("You might interest with these movie:");
+        System.out.println("-------------------------------------------");
+
+        Vector<Movie> movies = movieType.getMovies();
+        for (int i = 0; i < user.getNumOfRec(); i++) {
+            movies.get(i).displayInfo();
+            System.out.println("-------------------------------------------");
+            if (i == movies.size())
+                break;
+        }
+        System.out.println("Top movie types you have watched and rated: "+movieType.getType()+" with total of "+ highestNumber+ " reaction");
+        System.out.println("===========================================\n");
+    }
+
+    public void displayRecByFavDirect() {
+        // get User watched list, and find the most watched movie director repeated in
+        // the movie list
+        Vector<Director> directors = new Vector<Director>();
+        Vector<Integer> numRepeated = new Vector<Integer>();
+        int highestNumber = 0;
+        for (Movie d : user.getWatchedMovie()) {
+            if (!directors.contains(d.getDirector())) {
+                directors.add(d.getDirector());
+                numRepeated.add(1);
+                if (highestNumber < 1) {
+                    highestNumber = 1;
+                }
+            } else {
+                int index = directors.indexOf(d.getDirector());
+                int totalNum = numRepeated.get(index) + 1;
+                numRepeated.set(index, totalNum);
+                if (highestNumber < totalNum) {
+                    highestNumber = totalNum;
+                }
+            }
+        }
+        int mostRepeatedDirector_index = numRepeated.indexOf(highestNumber);
+        Director director = directors.get(mostRepeatedDirector_index);
+
+        System.out.println("===========================================");
+        System.out.println("From Favourite Director");
+        System.out.println("-------------------------------------------");
+        Vector<Movie> mVector = director.getMovie();
+        for (int i = 0; i < user.getNumOfRec(); i++) {
+            Movie m = mVector.get(i);
+            m.displayInfo();
+            System.out.println("-------------------------------------------");
+            if (i == mVector.size())
+                break;
+        }
+        System.out.println("You have watched "+highestNumber+" movies from "+director.getName());
+        System.out.println("===========================================\n");
+    }
+}
