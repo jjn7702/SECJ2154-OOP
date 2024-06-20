aa11d60f999108439aa0625834b17685ae89fd99
Hamdan saleh <147373027+hamdan-saleh@users.noreply.github.com>
2024-06-20 19:24:42 +0800

    Create shortTermOpportunity.java

---
+++ Submission/sec04_23242/Group 7/shortTermOpportunity.java
@@ -0,0 +1,5 @@
+public class shortTermOpportunity extends Opportunity {
+    public shortTermOpportunity(String title, String description, String date, String location,String organizationName) {
+        super(title, description, date, location, organizationName);
+    }
+}
