79c3db2db6988281fceef1d2f151f3285dd07850
Kughanraj Arunasalam <147372879+Kughanraj@users.noreply.github.com>
2024-06-21 02:00:17 +0800

    Update GrocerySystem.java

--- Submission/sec04_23242/Group2/Source_Code/GrocerySystem.java
+++ Submission/sec04_23242/Group2/Source_Code/GrocerySystem.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class GrocerySystem {
     public static void main(String[] args) throws IOException {
 
         ArrayList<User> users = new ArrayList<>();
