867c9e1e9b54040363be57364f593c82f79b28ed
Sarah Sofea <147372838+sarahsofea@users.noreply.github.com>
2024-06-22 23:44:24 +0800

    Update Sport.java

--- Submission/sec04_23242/JAVACADAVRA/source_code/Sport.java
+++ Submission/sec04_23242/JAVACADAVRA/source_code/Sport.java
@@ -1,1 +1,22 @@
+class Sport {
+    private String sportName, category;
+
+    public Sport(String sportName, String category) {
+        this.sportName = sportName;
+        this.category = category;
+    }
+
+    public String getSportName() {
+        return sportName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String toString() {
+        return "Sport Name: " + sportName + "\nCategory: " + category;
+    }
+
+}
 
