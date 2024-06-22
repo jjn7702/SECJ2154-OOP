8b7257c3abb9ff9b8d407ac08d4f60e0e375e06e
Tengku Aiman <147373281+Tmanlip@users.noreply.github.com>
2024-06-22 09:27:00 +0800

    update

--- Submission/sec04_23242/WHALE/source code/ScholarshipApp.java
+++ Submission/sec04_23242/WHALE/source code/ScholarshipApp.java
@@ -75,10 +75,10 @@ public class ScholarshipApp {
                                 System.out.println("There is no administrator signed in. Please wait");
                             }
 
-                            else if (ad.getStudentRej().contains(stu)) {
+                            else if (openFileRej(stu.getfName())) {
                                 System.out.println("Your application is not approved.");
 
-                            } else if (ad.getStudentApro().contains(stu)) {
+                            } else if (openFileRej(stu.getfName())) {
                                 System.out.println("Your application has approved. We will display the information");
                                 StudList.get(i).displayAllDetails();
                             } else {
@@ -630,7 +630,7 @@ public class ScholarshipApp {
         try (PrintWriter writer = new PrintWriter(new PrintWriter("ApprovedStudent.txt"))) {
             for (Student o : s) {
                 writer.write("Student Details:\n");
-                writer.write("Full Name: " + o.getfName() + " " + o.getlName());
+                writer.write("Full Name: " + o.getfName() + " " + o.getlName() + "\n");
                 writer.write("Age: " + o.getAge() + "\n");
                 writer.write("Email: " + o.getEmail() + "\n");
                 writer.write("Address: " + o.getAdd().toString() + "\n");
@@ -659,7 +659,7 @@ public class ScholarshipApp {
         try (PrintWriter writer = new PrintWriter(new PrintWriter("RejectedStudent.txt"))) {
             for (Student o : s) {
                 writer.write("Student Details:\n");
-                writer.write("Full Name: " + o.getfName() + " " + o.getlName());
+                writer.write("Full Name: " + o.getfName() + " " + o.getlName() + "\n");
                 writer.write("Age: " + o.getAge() + "\n");
                 writer.write("Email: " + o.getEmail() + "\n");
                 writer.write("Address: " + o.getAdd().toString() + "\n");
@@ -682,5 +682,55 @@ public class ScholarshipApp {
             System.err.println("Error writing to file: " + e.getMessage());
         }
 
+    }
+
+    private static boolean openFileApro(String fy){
+        try {
+            Scanner sc = new Scanner(new File("ApprovedStudent.txt")) ;
+            while(sc.hasNextLine()){
+                String op = sc.nextLine() ;
+
+                if (op.contains(fy)){
+                    return true ;
+                }
+
+                else{
+
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("There is no information about you");
+            // If file is not found, continue to next authentication attempt
+            System.err.println("Error: Unable to create or write to the file.");
+            e.printStackTrace() ;
+        }
+
+        return false ;
+    }
+
+    private static boolean openFileRej(String fy){
+        try {
+            Scanner sc = new Scanner(new File("ApprovedStudent.txt")) ;
+            while(sc.hasNextLine()){
+                String op = sc.nextLine() ;
+
+                if (op.contains(fy)){
+                    return true ;
+                }
+
+                else{
+
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("There is no information about you");
+            // If file is not found, continue to next authentication attempt
+            System.err.println("Error: Unable to create or write to the file.");
+            e.printStackTrace() ;
+        }
+
+        return false ;
     }
 }
--- Submission/sec04_23242/WHALE/source code/cuba.java
+++ Submission/sec04_23242/WHALE/source code/cuba.java
@@ -1,23 +1,38 @@
 import java.util.*;
+import java.io.* ;
 
 public class cuba{
     public static void main(String[] args) {
-        Scanner inp = new Scanner (System.in) ;
-        System.out.println("[0] Do you want to check the status of your apllication?");
-        System.out.println("[1] Do you want to apply for a scholarship?");
-        int ap ;
 
-                    try {
-                        ap = inp.nextInt() ;
-                        switch(ap){
-                            case 0 : System.out.println("dssd") ;
-                                    break ;
-                            case 1 : System.out.println("sfjkf") ;
-                        }
-                    }
-                      catch (InputMismatchException e) {
-                        System.out.println("Invalid Input. Please Enter a Number!");
-                        inp.nextLine();
-                    }
+        String p = "pppp" ;
+
+        boolean w = openFileApro(p) ;
+
+        System.out.println(w) ;
+    }
+
+    private static boolean openFileApro(String fy){
+        try {
+            Scanner sc = new Scanner(new File("ApprovedStudent.txt")) ;
+            while(sc.hasNextLine()){
+                String op = sc.nextLine() ;
+
+                if (op.contains(fy)){
+                    return true ;
+                }
+
+                else{
+
+                }
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("There is no information about you");
+            // If file is not found, continue to next authentication attempt
+            System.err.println("Error: Unable to create or write to the file.");
+            e.printStackTrace() ;
+        }
+
+        return false ;
     }
 }
