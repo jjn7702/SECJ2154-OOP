0200120c5d22804c4818023c529347ecf0d2b3b8
Farah Hazirah Nisha <147372967+farahhazirah@users.noreply.github.com>
2024-06-22 20:59:05 +0800

    Update Person.java

--- Submission/sec04_23242/JAVACADAVRA/source_code/Person.java
+++ Submission/sec04_23242/JAVACADAVRA/source_code/Person.java
@@ -1,13 +1,27 @@
-class Person{
-    private String name;
+
+class Person {
+    private String name, gender;
     private int age;
 
-    public Person(String name, int age){
+    public Person(String name, String gender, int age) {
         this.name = name;
+        this.gender = gender;
         this.age = age;
     }
 
-    public String getInfo(){
-        return "Name: " + name + ", Age: " + age ;
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getInfo() {
+        return "Name: " + name + ", Age: " + age + "\nGender: " + gender;
     }
 }
